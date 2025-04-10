780c72d9e7657b4cc3600c35abef0a27f922641e
szubarau <zumich92@gmail.com>
2025-04-10 12:23:59 +0300

    add config

---
+++ config.py
@@ -0,0 +1,6 @@
+Base_url = 'https://rahulshettyacademy.com'
+Key = '?key=qaclick123'
+Resource_Post = '/maps/api/place/add/json'
+Resource_Get = '/maps/api/place/get/json'
+Resource_Put = '/maps/api/place/update/json'
+Resource_Delete = '/maps/api/place/delete/json'
